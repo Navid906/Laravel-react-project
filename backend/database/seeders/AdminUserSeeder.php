7c0bcf8690815a2cb57f1503b3fa3adf135f4ddd
Navid Khan <navidnavid906@gmail.com>
2026-05-15 20:37:54 +0600

    Fixed migration and database seeding issues

--- backend/database/seeders/AdminUserSeeder.php
+++ backend/database/seeders/AdminUserSeeder.php
@@ -3,9 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-
 use App\Models\User;
-
 use Illuminate\Support\Facades\Hash;
 
 class AdminUserSeeder extends Seeder
@@ -13,39 +11,27 @@ class AdminUserSeeder extends Seeder
     public function run(): void
     {
         User::updateOrCreate(
-
             [
                 'email' => 'admin@example.com'
             ],
-
             [
                 'name' => 'Admin User',
-
                 'password' => Hash::make('password'),
-
                 'role' => 'admin',
-
                 'phone' => '0123456789',
-
                 'address' => 'Dhaka',
             ]
         );
 
         User::updateOrCreate(
-
             [
-                'email' => 'user@example.com'
+                'email' => 'JohnDoe@example.com'
             ],
-
             [
                 'name' => 'Normal User',
-
                 'password' => Hash::make('password'),
-
                 'role' => 'user',
-
                 'phone' => '01999999999',
-
                 'address' => 'Dhaka',
             ]
         );
